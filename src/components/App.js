81c176e201799f48ea19b85fc744abc3415e5081
Jay Davis <jdlikesbsbl@gmail.com>
2025-02-18 12:52:18 -0600

    adds About.js, ArticleList.js, Article.js, and Header.js and adds them as children to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,22 @@
 import React from "react";
 import blogData from "../data/blog";
+import Header from "../Header.js";
+import About from "../About.js"
+import ArticleList from "../ArticleList"
 
 console.log(blogData);
+
+const nameData = blogData.name;
+const aboutData = blogData.about;
+const imageData = blogData.image || "https://via.placeholder.com/215";
+const postsData = blogData.posts;
 
 function App() {
   return (
     <div className="App">
-      You're on your own from here! Follow the deliverables; test things out in
-      the browser as you write your code; and good luck!
+      <Header name={nameData} />
+      <About image={imageData} about={aboutData}/>
+      <ArticleList posts={postsData}/>
     </div>
   );
 }
